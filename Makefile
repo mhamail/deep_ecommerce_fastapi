9fa60dce3007b67598403633f7f5786f7432ea64
sbshamail123 <sbshamail123@gmail.com>
2026-03-10 12:24:27 +0500

    first commit

---
+++ Makefile
@@ -0,0 +1,29 @@
+SHELL := /bin/bash
+
+run:
+	uv run -- uvicorn src.main:app --reload --reload-dir src
+
+runip:
+	uv run -- uvicorn src.main:app --reload --host 0.0.0.0 --port 8000 --reload-dir src
+
+
+
+generate:
+	alembic revision --autogenerate -m "auto"
+
+downgrade:
+	alembic downgrade -1"
+
+head:
+	alembic upgrade head
+
+reset:
+	rm -rf .venv
+	uv venv .venv
+	source .venv/bin/activate && uv pip install -r pyproject.toml && uv pip install psycopg2-binary
+
+gitpush:
+	git add . && git commit -m "auto" && git push
+
+
+
---
+++ src/main.py
@@ -0,0 +1,40 @@
+# Initialize the FastAPI app with the custom lifespan
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Runs once on startup ---
+    # print("🟢 Checking if tables exist...")
+
+    # # Create all tables that are missing (safe – only creates non-existent ones)
+    # SQLModel.metadata.create_all(engine)
+    # print("✅ All tables verified / created.")
+    # # --- Runs once on shutdown ---
+    # print("🔴 App shutting down...")
+
+    yield  # 👈 after this, FastAPI starts handling requests
+
+
+app = FastAPI(lifespan=lifespan, root_path="/api")
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:3003",
+    ],  # or "*"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, FastAPI with uv!"}
