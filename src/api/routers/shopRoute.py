f71368272688a53c06b7023d480cb03dd99233c8
sbshamail123 <sbshamail123@gmail.com>
2026-04-02 18:31:45 +0500

    auto

--- src/api/routers/shopRoute.py
+++ src/api/routers/shopRoute.py
@@ -91,6 +91,48 @@ def findOne(
     return api_response(200, "Shop Found", data)
 
 
+# @router.delete("/delete/{id}", response_model=dict)
+# def delete_role(
+#     id: int,
+#     session: GetSession,
+#     user=requirePermission("shop_delete"),
+# ):
+#     user_id = user.get("id")
+
+#     shop = session.get(Shop, id)
+
+#     raiseExceptions((shop, 404, "Shop Data not found"))
+#     if ride.user_id != user_id:
+#         return api_response(403, "You are not allowed to update this ride")
+
+#     filenames_to_delete = []
+#     # -------------------------
+#     # CAR PIC
+#     # -------------------------
+#     if ride.car_pic and isinstance(ride.car_pic, dict):
+#         filename = ride.car_pic.get("filename")
+#         if filename:
+#             filenames_to_delete.append(filename)
+
+#     # -------------------------
+#     # OTHER IMAGES
+#     # -------------------------
+#     if isinstance(ride.other_images, List) and ride.other_images:
+#         for img in ride.other_images:
+#             if isinstance(img, dict) and img.get("filename"):
+#                 filenames_to_delete.append(img["filename"])
+
+#     # -------------------------
+#     # DELETE MEDIA FILES
+#     # -------------------------
+#     if filenames_to_delete:
+#         delete_media_items(session, filenames=filenames_to_delete)
+
+#     session.delete(ride)
+#     session.commit()
+#     return api_response(200, f"Ride {ride.id} deleted")
+
+
 @router.get("/list", response_model=list[ShopReadWithOwner])
 def list(
     query_params: ListQueryParams,
